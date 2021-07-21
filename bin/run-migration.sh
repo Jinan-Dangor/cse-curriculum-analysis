5decad8fc4e6ef4a91b89731d0be01dbdd345753
Jinan-Dangor <andrew@trouty.com>
2021-07-22 01:19:51 +1000

    Added button to insert eclips data into database

--- bin/run-migration.sh
+++ bin/run-migration.sh
@@ -1,2 +1,3 @@
-docker cp server/database/V20201005__1_support_both_grad.sql csecurriculumanalysis_db_1:/docker-entrypoint-initdb.d/V20201005__1_support_both_grad.sql
-docker exec -u postgres csecurriculumanalysis_db_1 psql cse-curriculum-analysis postgres -f /docker-entrypoint-initdb.d/V20201005__1_support_both_grad.sql
+# Replace file names to migrate database
+docker cp server/database/V20210708__1_eclips_overhaul.sql cse-curriculum-analysis_db_1:/docker-entrypoint-initdb.d/V20210708__1_eclips_overhaul.sql
+docker exec -u postgres cse-curriculum-analysis_db_1 psql cse-curriculum-analysis postgres -f /docker-entrypoint-initdb.d/V20210708__1_eclips_overhaul.sql
